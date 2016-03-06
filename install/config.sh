6006e8111274a9efcc94099bd4b5d184d94316cc
Rahul Malik <rahul.send89@gmail.com>
2016-03-06 23:28:00 +0530

    updated config.sh

--- install/config.sh
+++ install/config.sh
@@ -4,15 +4,6 @@ if test ! $(which brew); then
     echo "Installing homebrew"
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
-
-ln -s "${HOME}/.vim/vimrc ${HOMT}/.vimrc"
-ln -s "${HOME}/.vim/.xvimrc ${HOMT}/.xvimrc"
-ln -s "${HOME}/.vim/.ideavimrc ${HOMT}/.ideavimrc"
-
-mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
-ln -s ~/.vim $XDG_CONFIG_HOME/nvim
-ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
-
 echo "Installing homebrew packages..."
 
 brew tap thoughtbot/formulae
@@ -30,5 +21,6 @@ brew install wget
 brew install tmux
 brew tap Goles/battery
 brew install battery
+brew install ctags
 
 exit 0
